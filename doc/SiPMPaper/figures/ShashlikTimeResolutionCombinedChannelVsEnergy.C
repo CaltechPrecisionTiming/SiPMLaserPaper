ee615aecc1e36ba7a656f9503aa0a90926f8f311
SiXie <Si.Xie@cern.ch>
2017-10-17 19:48:07 +0200

    update plots

--- doc/SiPMPaper/figures/ShashlikTimeResolutionCombinedChannelVsEnergy.C
+++ doc/SiPMPaper/figures/ShashlikTimeResolutionCombinedChannelVsEnergy.C
@@ -1,10 +1,8 @@
-void ShashlikTimeResolutionCombinedChannelVsEnergy()
 {
 //=========Macro generated from canvas: c1/c1
-//=========  (Sat Aug 19 19:56:13 2017) by ROOT version6.08/05
-   TCanvas *c1 = new TCanvas("c1", "c1",2,51,800,600);
-   gStyle->SetOptStat(0);
-   c1->Range(-38.46154,-30.82051,282.0513,226.0171);
+//=========  (Tue Oct 17 19:45:00 2017) by ROOT version5.34/36
+   TCanvas *c1 = new TCanvas("c1", "c1",6,103,800,600);
+   c1->Range(-38.46154,-30.76923,282.0513,225.641);
    c1->SetFillColor(0);
    c1->SetBorderMode(0);
    c1->SetBorderSize(2);
@@ -13,68 +11,111 @@ void ShashlikTimeResolutionCombinedChannelVsEnergy()
    c1->SetFrameBorderMode(0);
    c1->SetFrameBorderMode(0);
    
-   TH2F *DSB1__1 = new TH2F("DSB1__1","",250,0,250,600,0,200);
-   DSB1__1->SetBinContent(32205,1);
-   DSB1__1->SetBinContent(37195,1);
-   DSB1__1->SetBinContent(40421,1);
-   DSB1__1->SetBinContent(58263,1);
-   DSB1__1->SetEntries(4);
-   DSB1__1->SetStats(0);
+   TMultiGraph *multigraph = new TMultiGraph();
+   multigraph->SetName("multi1");
+   multigraph->SetTitle("");
+   
+   TGraphErrors *gre = new TGraphErrors(4);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
+   gre->SetLineWidth(2);
+   gre->SetMarkerStyle(20);
+   gre->SetMarkerSize(1.4);
+   gre->SetPoint(0,50,76.92387);
+   gre->SetPointError(0,0,2.330061);
+   gre->SetPoint(1,100,53.2474);
+   gre->SetPointError(1,0,1.924183);
+   gre->SetPoint(2,150,48.72874);
+   gre->SetPointError(2,0,1.126897);
+   gre->SetPoint(3,200,42.01855);
+   gre->SetPointError(3,0,2.131022);
+   
+   TH1F *Graph_Graph1 = new TH1F("Graph_Graph1","Graph",100,35,215);
+   Graph_Graph1->SetMinimum(35.95089);
+   Graph_Graph1->SetMaximum(83.19057);
+   Graph_Graph1->SetDirectory(0);
+   Graph_Graph1->SetStats(0);
 
    Int_t ci;      // for color index setting
    TColor *color; // for color definition with alpha
    ci = TColor::GetColor("#000099");
-   DSB1__1->SetLineColor(ci);
-   DSB1__1->SetMarkerStyle(20);
-   DSB1__1->SetMarkerSize(1.4);
-   DSB1__1->GetXaxis()->SetTitle("Energy [GeV]");
-   DSB1__1->GetXaxis()->SetLabelFont(42);
-   DSB1__1->GetXaxis()->SetLabelSize(0.05);
-   DSB1__1->GetXaxis()->SetTitleSize(0.05);
-   DSB1__1->GetXaxis()->SetTitleOffset(1.1);
-   DSB1__1->GetXaxis()->SetTitleFont(42);
-   DSB1__1->GetYaxis()->SetTitle("#sigma t [ps]");
-   DSB1__1->GetYaxis()->SetRange(1,601);
-   DSB1__1->GetYaxis()->SetLabelFont(42);
-   DSB1__1->GetYaxis()->SetLabelOffset(0.01);
-   DSB1__1->GetYaxis()->SetLabelSize(0.05);
-   DSB1__1->GetYaxis()->SetTitleSize(0.05);
-   DSB1__1->GetYaxis()->SetTitleOffset(1.2);
-   DSB1__1->GetYaxis()->SetTitleFont(42);
-   DSB1__1->GetZaxis()->SetLabelFont(42);
-   DSB1__1->GetZaxis()->SetLabelSize(0.035);
-   DSB1__1->GetZaxis()->SetTitleSize(0.035);
-   DSB1__1->GetZaxis()->SetTitleFont(42);
-   DSB1__1->Draw("");
+   Graph_Graph1->SetLineColor(ci);
+   Graph_Graph1->GetXaxis()->SetLabelFont(42);
+   Graph_Graph1->GetXaxis()->SetLabelSize(0.035);
+   Graph_Graph1->GetXaxis()->SetTitleSize(0.035);
+   Graph_Graph1->GetXaxis()->SetTitleFont(42);
+   Graph_Graph1->GetYaxis()->SetLabelFont(42);
+   Graph_Graph1->GetYaxis()->SetLabelSize(0.035);
+   Graph_Graph1->GetYaxis()->SetTitleSize(0.035);
+   Graph_Graph1->GetYaxis()->SetTitleFont(42);
+   Graph_Graph1->GetZaxis()->SetLabelFont(42);
+   Graph_Graph1->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph1->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph1->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph1);
    
-   TH2F *Cap__2 = new TH2F("Cap__2","1000*tree_Cap_time_1_2_3_4:tree_Energy {tree_Energy > 40}",250,0,250,600,0,200);
-   Cap__2->SetBinContent(55591,1);
-   Cap__2->SetBinContent(55641,1);
-   Cap__2->SetBinContent(65621,1);
-   Cap__2->SetBinContent(96819,1);
-   Cap__2->SetEntries(4);
-   Cap__2->SetStats(0);
-
-   ci = TColor::GetColor("#000099");
-   Cap__2->SetLineColor(ci);
+   multigraph->Add(gre,"");
+   
+   gre = new TGraphErrors(4);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
 
    ci = TColor::GetColor("#0000ff");
-   Cap__2->SetMarkerColor(ci);
-   Cap__2->SetMarkerStyle(34);
-   Cap__2->SetMarkerSize(1.8);
-   Cap__2->GetXaxis()->SetLabelFont(42);
-   Cap__2->GetXaxis()->SetLabelSize(0.035);
-   Cap__2->GetXaxis()->SetTitleSize(0.035);
-   Cap__2->GetXaxis()->SetTitleFont(42);
-   Cap__2->GetYaxis()->SetLabelFont(42);
-   Cap__2->GetYaxis()->SetLabelSize(0.035);
-   Cap__2->GetYaxis()->SetTitleSize(0.035);
-   Cap__2->GetYaxis()->SetTitleFont(42);
-   Cap__2->GetZaxis()->SetLabelFont(42);
-   Cap__2->GetZaxis()->SetLabelSize(0.035);
-   Cap__2->GetZaxis()->SetTitleSize(0.035);
-   Cap__2->GetZaxis()->SetTitleFont(42);
-   Cap__2->Draw("same");
+   gre->SetLineColor(ci);
+   gre->SetLineWidth(2);
+
+   ci = TColor::GetColor("#0000ff");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(34);
+   gre->SetMarkerSize(1.8);
+   gre->SetPoint(0,50,127.872);
+   gre->SetPointError(0,0,3.335863);
+   gre->SetPoint(1,100,86.43952);
+   gre->SetPointError(1,0,2.342178);
+   gre->SetPoint(2,150,73.11114);
+   gre->SetPointError(2,0,2.1924);
+   gre->SetPoint(3,200,73.28807);
+   gre->SetPointError(3,0,1.603901);
+   
+   TH1F *Graph_Graph2 = new TH1F("Graph_Graph2","Graph",100,35,215);
+   Graph_Graph2->SetMinimum(64.88983);
+   Graph_Graph2->SetMaximum(137.2368);
+   Graph_Graph2->SetDirectory(0);
+   Graph_Graph2->SetStats(0);
+
+   ci = TColor::GetColor("#000099");
+   Graph_Graph2->SetLineColor(ci);
+   Graph_Graph2->GetXaxis()->SetLabelFont(42);
+   Graph_Graph2->GetXaxis()->SetLabelSize(0.035);
+   Graph_Graph2->GetXaxis()->SetTitleSize(0.035);
+   Graph_Graph2->GetXaxis()->SetTitleFont(42);
+   Graph_Graph2->GetYaxis()->SetLabelFont(42);
+   Graph_Graph2->GetYaxis()->SetLabelSize(0.035);
+   Graph_Graph2->GetYaxis()->SetTitleSize(0.035);
+   Graph_Graph2->GetYaxis()->SetTitleFont(42);
+   Graph_Graph2->GetZaxis()->SetLabelFont(42);
+   Graph_Graph2->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph2->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph2->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph2);
+   
+   multigraph->Add(gre,"");
+   multigraph->Draw("AP");
+   multigraph->GetXaxis()->SetTitle("Energy [GeV]");
+   multigraph->GetXaxis()->SetLabelFont(42);
+   multigraph->GetXaxis()->SetLabelSize(0.05);
+   multigraph->GetXaxis()->SetTitleSize(0.05);
+   multigraph->GetXaxis()->SetTitleOffset(1.1);
+   multigraph->GetXaxis()->SetTitleFont(42);
+   multigraph->GetYaxis()->SetTitle("#sigma t [ps]");
+   multigraph->GetYaxis()->SetLabelFont(42);
+   multigraph->GetYaxis()->SetLabelOffset(0.01);
+   multigraph->GetYaxis()->SetLabelSize(0.05);
+   multigraph->GetYaxis()->SetTitleSize(0.05);
+   multigraph->GetYaxis()->SetTitleOffset(1.2);
+   multigraph->GetYaxis()->SetTitleFont(42);
    
    TLegend *leg = new TLegend(0.25,0.7,0.87,0.85,NULL,"brNDC");
    leg->SetBorderSize(1);
@@ -84,7 +125,7 @@ void ShashlikTimeResolutionCombinedChannelVsEnergy()
    leg->SetLineWidth(1);
    leg->SetFillColor(0);
    leg->SetFillStyle(1001);
-   TLegendEntry *entry=leg->AddEntry("DSB1","Combined DSB Fibers","p");
+   TLegendEntry *entry=leg->AddEntry("Graph","Combined DSB Fibers","p");
    entry->SetLineColor(1);
    entry->SetLineStyle(1);
    entry->SetLineWidth(1);
@@ -92,7 +133,7 @@ void ShashlikTimeResolutionCombinedChannelVsEnergy()
    entry->SetMarkerStyle(20);
    entry->SetMarkerSize(1.4);
    entry->SetTextFont(42);
-   entry=leg->AddEntry("Cap","Combined Quartz Capillaries","p");
+   entry=leg->AddEntry("Graph","Combined Quartz Capillaries","p");
    entry->SetLineColor(1);
    entry->SetLineStyle(1);
    entry->SetLineWidth(1);
--- doc/SiPMPaper/figures/ShashlikTimeResolutionVsEnergy_DSB.C
+++ doc/SiPMPaper/figures/ShashlikTimeResolutionVsEnergy_DSB.C
@@ -1,9 +1,7 @@
-void ShashlikTimeResolutionVsEnergy_DSB()
 {
 //=========Macro generated from canvas: c1/c1
-//=========  (Tue Aug 22 18:42:41 2017) by ROOT version6.08/05
-   TCanvas *c1 = new TCanvas("c1", "c1",1009,74,800,600);
-   gStyle->SetOptStat(0);
+//=========  (Tue Oct 17 19:31:17 2017) by ROOT version5.34/36
+   TCanvas *c1 = new TCanvas("c1", "c1",6,103,800,600);
    c1->Range(-38.46154,-30.76923,282.0513,225.641);
    c1->SetFillColor(0);
    c1->SetBorderMode(0);
@@ -13,129 +11,203 @@ void ShashlikTimeResolutionVsEnergy_DSB()
    c1->SetFrameBorderMode(0);
    c1->SetFrameBorderMode(0);
    
-   TH2F *DSB1ch1__5 = new TH2F("DSB1ch1__5","",250,0,250,600,0,400);
-   DSB1ch1__5->SetBinContent(24141,1);
-   DSB1ch1__5->SetBinContent(27871,1);
-   DSB1ch1__5->SetBinContent(35885,1);
-   DSB1ch1__5->SetBinContent(51711,1);
-   DSB1ch1__5->SetEntries(4);
-   DSB1ch1__5->SetStats(0);
+   TMultiGraph *multigraph = new TMultiGraph();
+   multigraph->SetName("multi1");
+   multigraph->SetTitle("");
+   
+   TGraphErrors *gre = new TGraphErrors(4);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
+   gre->SetLineWidth(2);
+   gre->SetMarkerStyle(20);
+   gre->SetMarkerSize(1.4);
+   gre->SetPoint(0,50,136.2525);
+   gre->SetPointError(0,0,3.282363);
+   gre->SetPoint(1,100,94.5824);
+   gre->SetPointError(1,0,2.869215);
+   gre->SetPoint(2,150,72.81868);
+   gre->SetPointError(2,0,1.797553);
+   gre->SetPoint(3,200,62.93952);
+   gre->SetPointError(3,0,1.908301);
+   
+   TH1F *Graph_Graph1 = new TH1F("Graph_Graph1","Graph",100,35,215);
+   Graph_Graph1->SetMinimum(53.18086);
+   Graph_Graph1->SetMaximum(147.3852);
+   Graph_Graph1->SetDirectory(0);
+   Graph_Graph1->SetStats(0);
 
    Int_t ci;      // for color index setting
    TColor *color; // for color definition with alpha
    ci = TColor::GetColor("#000099");
-   DSB1ch1__5->SetLineColor(ci);
-   DSB1ch1__5->SetMarkerStyle(20);
-   DSB1ch1__5->SetMarkerSize(1.4);
-   DSB1ch1__5->GetXaxis()->SetTitle("Energy [GeV]");
-   DSB1ch1__5->GetXaxis()->SetLabelFont(42);
-   DSB1ch1__5->GetXaxis()->SetLabelSize(0.05);
-   DSB1ch1__5->GetXaxis()->SetTitleSize(0.05);
-   DSB1ch1__5->GetXaxis()->SetTitleOffset(1.1);
-   DSB1ch1__5->GetXaxis()->SetTitleFont(42);
-   DSB1ch1__5->GetYaxis()->SetTitle("#sigma t [ps]");
-   DSB1ch1__5->GetYaxis()->SetRange(1,300);
-   DSB1ch1__5->GetYaxis()->SetLabelFont(42);
-   DSB1ch1__5->GetYaxis()->SetLabelOffset(0.01);
-   DSB1ch1__5->GetYaxis()->SetLabelSize(0.05);
-   DSB1ch1__5->GetYaxis()->SetTitleSize(0.05);
-   DSB1ch1__5->GetYaxis()->SetTitleOffset(1.2);
-   DSB1ch1__5->GetYaxis()->SetTitleFont(42);
-   DSB1ch1__5->GetZaxis()->SetLabelFont(42);
-   DSB1ch1__5->GetZaxis()->SetLabelSize(0.035);
-   DSB1ch1__5->GetZaxis()->SetTitleSize(0.035);
-   DSB1ch1__5->GetZaxis()->SetTitleFont(42);
-   DSB1ch1__5->Draw("");
+   Graph_Graph1->SetLineColor(ci);
+   Graph_Graph1->GetXaxis()->SetLabelFont(42);
+   Graph_Graph1->GetXaxis()->SetLabelSize(0.035);
+   Graph_Graph1->GetXaxis()->SetTitleSize(0.035);
+   Graph_Graph1->GetXaxis()->SetTitleFont(42);
+   Graph_Graph1->GetYaxis()->SetLabelFont(42);
+   Graph_Graph1->GetYaxis()->SetLabelSize(0.035);
+   Graph_Graph1->GetYaxis()->SetTitleSize(0.035);
+   Graph_Graph1->GetYaxis()->SetTitleFont(42);
+   Graph_Graph1->GetZaxis()->SetLabelFont(42);
+   Graph_Graph1->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph1->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph1->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph1);
    
-   TH2F *DSB1ch2__6 = new TH2F("DSB1ch2__6","1000*tree_DSB1_time_2:tree_Energy {tree_Energy>40}",250,0,250,600,0,400);
-   DSB1ch2__6->SetBinContent(23637,1);
-   DSB1ch2__6->SetBinContent(25099,1);
-   DSB1ch2__6->SetBinContent(30593,1);
-   DSB1ch2__6->SetBinContent(36087,1);
-   DSB1ch2__6->SetEntries(4);
-   DSB1ch2__6->SetStats(0);
-
-   ci = TColor::GetColor("#000099");
-   DSB1ch2__6->SetLineColor(ci);
+   multigraph->Add(gre,"");
+   
+   gre = new TGraphErrors(4);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
 
    ci = TColor::GetColor("#0000ff");
-   DSB1ch2__6->SetMarkerColor(ci);
-   DSB1ch2__6->SetMarkerStyle(21);
-   DSB1ch2__6->SetMarkerSize(1.4);
-   DSB1ch2__6->GetXaxis()->SetLabelFont(42);
-   DSB1ch2__6->GetXaxis()->SetLabelSize(0.035);
-   DSB1ch2__6->GetXaxis()->SetTitleSize(0.035);
-   DSB1ch2__6->GetXaxis()->SetTitleFont(42);
-   DSB1ch2__6->GetYaxis()->SetRange(7,294);
-   DSB1ch2__6->GetYaxis()->SetLabelFont(42);
-   DSB1ch2__6->GetYaxis()->SetLabelSize(0.035);
-   DSB1ch2__6->GetYaxis()->SetTitleSize(0.035);
-   DSB1ch2__6->GetYaxis()->SetTitleFont(42);
-   DSB1ch2__6->GetZaxis()->SetLabelFont(42);
-   DSB1ch2__6->GetZaxis()->SetLabelSize(0.035);
-   DSB1ch2__6->GetZaxis()->SetTitleSize(0.035);
-   DSB1ch2__6->GetZaxis()->SetTitleFont(42);
-   DSB1ch2__6->Draw("same");
+   gre->SetLineColor(ci);
+   gre->SetLineWidth(2);
+
+   ci = TColor::GetColor("#0000ff");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(21);
+   gre->SetMarkerSize(1.4);
+   gre->SetPoint(0,50,95.33232);
+   gre->SetPointError(0,0,2.289859);
+   gre->SetPoint(1,100,80.40045);
+   gre->SetPointError(1,0,2.466177);
+   gre->SetPoint(2,150,65.34431);
+   gre->SetPointError(2,0,1.19624);
+   gre->SetPoint(3,200,61.70853);
+   gre->SetPointError(3,0,2.064112);
    
-   TH2F *DSB1ch3__7 = new TH2F("DSB1ch3__7","1000*tree_DSB1_time_3:tree_Energy {tree_Energy>40}",250,0,250,1200,0,800);
-   DSB1ch3__7->SetBinContent(29181,1);
-   DSB1ch3__7->SetBinContent(30391,1);
-   DSB1ch3__7->SetBinContent(35129,1);
-   DSB1ch3__7->SetBinContent(45915,1);
-   DSB1ch3__7->SetEntries(4);
-   DSB1ch3__7->SetStats(0);
+   TH1F *Graph_Graph2 = new TH1F("Graph_Graph2","Graph",100,35,215);
+   Graph_Graph2->SetMinimum(55.84664);
+   Graph_Graph2->SetMaximum(101.42);
+   Graph_Graph2->SetDirectory(0);
+   Graph_Graph2->SetStats(0);
 
    ci = TColor::GetColor("#000099");
-   DSB1ch3__7->SetLineColor(ci);
+   Graph_Graph2->SetLineColor(ci);
+   Graph_Graph2->GetXaxis()->SetLabelFont(42);
+   Graph_Graph2->GetXaxis()->SetLabelSize(0.035);
+   Graph_Graph2->GetXaxis()->SetTitleSize(0.035);
+   Graph_Graph2->GetXaxis()->SetTitleFont(42);
+   Graph_Graph2->GetYaxis()->SetLabelFont(42);
+   Graph_Graph2->GetYaxis()->SetLabelSize(0.035);
+   Graph_Graph2->GetYaxis()->SetTitleSize(0.035);
+   Graph_Graph2->GetYaxis()->SetTitleFont(42);
+   Graph_Graph2->GetZaxis()->SetLabelFont(42);
+   Graph_Graph2->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph2->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph2->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph2);
+   
+   multigraph->Add(gre,"");
+   
+   gre = new TGraphErrors(4);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
 
    ci = TColor::GetColor("#ff0000");
-   DSB1ch3__7->SetMarkerColor(ci);
-   DSB1ch3__7->SetMarkerStyle(34);
-   DSB1ch3__7->SetMarkerSize(1.4);
-   DSB1ch3__7->GetXaxis()->SetLabelFont(42);
-   DSB1ch3__7->GetXaxis()->SetLabelSize(0.035);
-   DSB1ch3__7->GetXaxis()->SetTitleSize(0.035);
-   DSB1ch3__7->GetXaxis()->SetTitleFont(42);
-   DSB1ch3__7->GetYaxis()->SetRange(7,294);
-   DSB1ch3__7->GetYaxis()->SetLabelFont(42);
-   DSB1ch3__7->GetYaxis()->SetLabelSize(0.035);
-   DSB1ch3__7->GetYaxis()->SetTitleSize(0.035);
-   DSB1ch3__7->GetYaxis()->SetTitleFont(42);
-   DSB1ch3__7->GetZaxis()->SetLabelFont(42);
-   DSB1ch3__7->GetZaxis()->SetLabelSize(0.035);
-   DSB1ch3__7->GetZaxis()->SetTitleSize(0.035);
-   DSB1ch3__7->GetZaxis()->SetTitleFont(42);
-   DSB1ch3__7->Draw("same");
+   gre->SetLineColor(ci);
+   gre->SetLineWidth(2);
+
+   ci = TColor::GetColor("#ff0000");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(34);
+   gre->SetMarkerSize(1.4);
+   gre->SetPoint(0,50,121.1411);
+   gre->SetPointError(0,0,2.166436);
+   gre->SetPoint(1,100,92.54782);
+   gre->SetPointError(1,0,2.669427);
+   gre->SetPoint(2,150,79.38335);
+   gre->SetPointError(2,0,1.591395);
+   gre->SetPoint(3,200,76.43974);
+   gre->SetPointError(3,0,4.618151);
    
-   TH2F *DSB1ch4__8 = new TH2F("DSB1ch4__8","1000*tree_DSB1_time_4:tree_Energy {tree_Energy>40}",250,0,250,600,0,400);
-   DSB1ch4__8->SetBinContent(23839,1);
-   DSB1ch4__8->SetBinContent(24141,1);
-   DSB1ch4__8->SetBinContent(27821,1);
-   DSB1ch4__8->SetBinContent(35079,1);
-   DSB1ch4__8->SetEntries(4);
-   DSB1ch4__8->SetStats(0);
+   TH1F *Graph_Graph3 = new TH1F("Graph_Graph3","Graph",100,35,215);
+   Graph_Graph3->SetMinimum(66.67299);
+   Graph_Graph3->SetMaximum(128.4561);
+   Graph_Graph3->SetDirectory(0);
+   Graph_Graph3->SetStats(0);
 
    ci = TColor::GetColor("#000099");
-   DSB1ch4__8->SetLineColor(ci);
+   Graph_Graph3->SetLineColor(ci);
+   Graph_Graph3->GetXaxis()->SetLabelFont(42);
+   Graph_Graph3->GetXaxis()->SetLabelSize(0.035);
+   Graph_Graph3->GetXaxis()->SetTitleSize(0.035);
+   Graph_Graph3->GetXaxis()->SetTitleFont(42);
+   Graph_Graph3->GetYaxis()->SetLabelFont(42);
+   Graph_Graph3->GetYaxis()->SetLabelSize(0.035);
+   Graph_Graph3->GetYaxis()->SetTitleSize(0.035);
+   Graph_Graph3->GetYaxis()->SetTitleFont(42);
+   Graph_Graph3->GetZaxis()->SetLabelFont(42);
+   Graph_Graph3->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph3->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph3->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph3);
+   
+   multigraph->Add(gre,"");
+   
+   gre = new TGraphErrors(4);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
 
    ci = TColor::GetColor("#009900");
-   DSB1ch4__8->SetMarkerColor(ci);
-   DSB1ch4__8->SetMarkerStyle(22);
-   DSB1ch4__8->SetMarkerSize(1.4);
-   DSB1ch4__8->GetXaxis()->SetLabelFont(42);
-   DSB1ch4__8->GetXaxis()->SetLabelSize(0.035);
-   DSB1ch4__8->GetXaxis()->SetTitleSize(0.035);
-   DSB1ch4__8->GetXaxis()->SetTitleFont(42);
-   DSB1ch4__8->GetYaxis()->SetRange(7,294);
-   DSB1ch4__8->GetYaxis()->SetLabelFont(42);
-   DSB1ch4__8->GetYaxis()->SetLabelSize(0.035);
-   DSB1ch4__8->GetYaxis()->SetTitleSize(0.035);
-   DSB1ch4__8->GetYaxis()->SetTitleFont(42);
-   DSB1ch4__8->GetZaxis()->SetLabelFont(42);
-   DSB1ch4__8->GetZaxis()->SetLabelSize(0.035);
-   DSB1ch4__8->GetZaxis()->SetTitleSize(0.035);
-   DSB1ch4__8->GetZaxis()->SetTitleFont(42);
-   DSB1ch4__8->Draw("same");
+   gre->SetLineColor(ci);
+   gre->SetLineWidth(2);
+
+   ci = TColor::GetColor("#009900");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(22);
+   gre->SetMarkerSize(1.4);
+   gre->SetPoint(0,50,92.25575);
+   gre->SetPointError(0,0,2.167213);
+   gre->SetPoint(1,100,73.02962);
+   gre->SetPointError(1,0,2.1609);
+   gre->SetPoint(2,150,62.29272);
+   gre->SetPointError(2,0,1.36683);
+   gre->SetPoint(3,200,62.67965);
+   gre->SetPointError(3,0,2.283515);
+   
+   TH1F *Graph_Graph4 = new TH1F("Graph_Graph4","Graph",100,35,215);
+   Graph_Graph4->SetMinimum(56.99345);
+   Graph_Graph4->SetMaximum(97.82565);
+   Graph_Graph4->SetDirectory(0);
+   Graph_Graph4->SetStats(0);
+
+   ci = TColor::GetColor("#000099");
+   Graph_Graph4->SetLineColor(ci);
+   Graph_Graph4->GetXaxis()->SetLabelFont(42);
+   Graph_Graph4->GetXaxis()->SetLabelSize(0.035);
+   Graph_Graph4->GetXaxis()->SetTitleSize(0.035);
+   Graph_Graph4->GetXaxis()->SetTitleFont(42);
+   Graph_Graph4->GetYaxis()->SetLabelFont(42);
+   Graph_Graph4->GetYaxis()->SetLabelSize(0.035);
+   Graph_Graph4->GetYaxis()->SetTitleSize(0.035);
+   Graph_Graph4->GetYaxis()->SetTitleFont(42);
+   Graph_Graph4->GetZaxis()->SetLabelFont(42);
+   Graph_Graph4->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph4->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph4->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph4);
+   
+   multigraph->Add(gre,"");
+   multigraph->Draw("AP");
+   multigraph->GetXaxis()->SetTitle("Energy [GeV]");
+   multigraph->GetXaxis()->SetLabelFont(42);
+   multigraph->GetXaxis()->SetLabelSize(0.05);
+   multigraph->GetXaxis()->SetTitleSize(0.05);
+   multigraph->GetXaxis()->SetTitleOffset(1.1);
+   multigraph->GetXaxis()->SetTitleFont(42);
+   multigraph->GetYaxis()->SetTitle("#sigma t [ps]");
+   multigraph->GetYaxis()->SetLabelFont(42);
+   multigraph->GetYaxis()->SetLabelOffset(0.01);
+   multigraph->GetYaxis()->SetLabelSize(0.05);
+   multigraph->GetYaxis()->SetTitleSize(0.05);
+   multigraph->GetYaxis()->SetTitleOffset(1.2);
+   multigraph->GetYaxis()->SetTitleFont(42);
    
    TLegend *leg = new TLegend(0.4,0.65,0.88,0.88,NULL,"brNDC");
    leg->SetBorderSize(1);
@@ -145,7 +217,7 @@ void ShashlikTimeResolutionVsEnergy_DSB()
    leg->SetLineWidth(1);
    leg->SetFillColor(0);
    leg->SetFillStyle(1001);
-   TLegendEntry *entry=leg->AddEntry("DSB1ch1","SiPM 3x3 mm^{2}, 15 #mu m pitch","p");
+   TLegendEntry *entry=leg->AddEntry("Graph","SiPM 3x3 mm^{2}, 15 #mu m pitch","p");
    entry->SetLineColor(1);
    entry->SetLineStyle(1);
    entry->SetLineWidth(1);
@@ -153,7 +225,7 @@ void ShashlikTimeResolutionVsEnergy_DSB()
    entry->SetMarkerStyle(20);
    entry->SetMarkerSize(1.4);
    entry->SetTextFont(42);
-   entry=leg->AddEntry("DSB1ch2","SiPM 3x3 mm^{2}, 25 #mu m pitch","p");
+   entry=leg->AddEntry("Graph","SiPM 3x3 mm^{2}, 25 #mu m pitch","p");
    entry->SetLineColor(1);
    entry->SetLineStyle(1);
    entry->SetLineWidth(1);
@@ -163,7 +235,7 @@ void ShashlikTimeResolutionVsEnergy_DSB()
    entry->SetMarkerStyle(21);
    entry->SetMarkerSize(1.4);
    entry->SetTextFont(42);
-   entry=leg->AddEntry("DSB1ch3","SiPM 1x1 mm^{2}, 10 #mu m pitch","p");
+   entry=leg->AddEntry("Graph","SiPM 1x1 mm^{2}, 10 #mu m pitch","p");
    entry->SetLineColor(1);
    entry->SetLineStyle(1);
    entry->SetLineWidth(1);
@@ -173,7 +245,7 @@ void ShashlikTimeResolutionVsEnergy_DSB()
    entry->SetMarkerStyle(34);
    entry->SetMarkerSize(1.4);
    entry->SetTextFont(42);
-   entry=leg->AddEntry("DSB1ch4","SiPM 1x1 mm^{2}  15 #mu m pitch","p");
+   entry=leg->AddEntry("Graph","SiPM 1x1 mm^{2}  15 #mu m pitch","p");
    entry->SetLineColor(1);
    entry->SetLineStyle(1);
    entry->SetLineWidth(1);
